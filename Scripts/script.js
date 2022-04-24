84647b205d9b07212ed67f5b6121924b6f8b0555
MathRMM <matheusrodrigomm@gmail.com>
2022-04-24 19:36:21 -0300

    backup troca de tela

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,39 +1,51 @@
 //Tela 1 Principal
 //difinições
-const contain_style = document.querySelector("style");
-const contain_ul = document.querySelector("ul");
-const API = "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes";
+const API = "https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes";
+const main = document.querySelector("main");
 const elementoQueQueroQueApareca = document.querySelector("ul");
+let lista_quizz_usuario =[]
 
 let img_quizz;
 let r = 0;
 
 //funções
 function iniciar_site() {
-    lista_quizz();
+    
+    main.innerHTML = `
+        <div class="tela_1">
+            <div class="superior">
+            </div>
+            <div class="lista-quizz">
+                <div class="titulo-lista-quizz">
+                    <h2>Todos os Quizzes</h2>
+                </div>
+                <ul>
+                </ul>
+            </div>
+        </div>  
+    `
+    lista_quizz()
+    selecionar_tela_quizzusuario();
 }
 
 function lista_quizz() {
-   const promisse = axios.get(`${API}`).then(implementar_quizz);
-   
+  axios.get(`${API}`).then(implementar_quizz);  
 }
 
 function implementar_quizz(promisse) {
+    const contain_ul = document.querySelector("ul");
     for (let i = 0; i < 12; i++) {
         let listar_img_quizz = promisse.data[i].image;
         let listar_title_quizz = promisse.data[i].title;
         let listar_id_quizz = promisse.data[i].id;
-        contain_style.innerHTML += ` 
-                div.lista-quizz div#a${listar_id_quizz}.quizz{
-                    background:linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 64.58%, #000000 100%),
+
+        contain_ul.innerHTML += `
+                <div id="quizz_${listar_id_quizz}" class="quizz" onClick = Mudar_tela() style="
+                background:linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 64.58%, #000000 100%),
                     url(${listar_img_quizz});
                     background-size: cover;
                     background-position: center;
-                          
-                }`;
-
-        contain_ul.innerHTML += `
-                <div id="a${listar_id_quizz}" class="quizz" onClick = Mudar_tela()>
+                ">
                     <h3>${listar_title_quizz}</h3>
                 </div>
             `;
@@ -41,16 +53,66 @@ function implementar_quizz(promisse) {
     }
 }
 
-function Mudar_tela(){
-    resetar_tela()
-}
-
-function resetar_tela(){
-    const main = document.querySelector(".tela_1")
-    if (main.parentNode){
-        main.parentNode.removeChild(main)
+function selecionar_tela_quizzusuario(){
+    
+    if(lista_quizz_usuario[0] == !undefined){
+        com_quizz_usuario();
+    }else {
+        sem_quizz_usuario();
     }
 }
+
+function sem_quizz_usuario(){
+    const contain_semQuizz = document.querySelector(".superior")
+    contain_semQuizz.innerHTML += `
+    <div class="criar-quizz">
+          <div class="texto">
+            <span> Você nâo criou nenhum quizz ainda :(</span> 
+          </div>
+          <div class="botao" onClick= Mudar_tela_criar()>Criar Quizz</div>
+        </div>
+    `;
+}
+
+function com_quizz_usuario(){
+    const contain_comQuiz = document.querySelector(".superior")
+    contain_comQuiz.innerHTML += `
+    <div class="quizz_usuario">
+          <h2>Seus Quizzes</h2>
+          <span>
+            <ion-icon name="add-circle-sharp" onclick="Mudar_tela_criar()"></ion-icon>
+          </span>
+            <div id="a8097" class="quizz" onclick="Mudar_tela()">
+              <h3>League of Legends: O teste definitivo</h3>
+            </div>
+            <div id="a8097" class="quizz" onclick="Mudar_tela()">
+              <h3>League of Legends: O teste definitivo</h3>
+            </div>
+           
+            
+        </div>
+    `;
+}
+
+function Mudar_tela_criar(){
+    resetar_tela1_style()
+}
+
+function Mudar_tela_quizz(){
+    resetar_tela1_style()
+}
+
+function resetar_tela1_style(){
+    const tela_1 = document.querySelector(".tela_1")
+    if (tela_1.parentNode){
+        tela_1.parentNode.removeChild(tela_1)
+    }
+  
+}
+
+
+
+
 
 
 //Tela 2 - Quizz
@@ -73,6 +135,7 @@ function selecionarResposta(elemento) {
     }
 }
 
-
+// Tela 3 - Montar quizz do Usuario
 //chamar função
 iniciar_site();
+//Mudar_tela_criar();
