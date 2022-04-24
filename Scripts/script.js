7bc2917c66aa95ba7be620ffe1a0e5ae48977660
ruantoninatto <ruantoninatto@gmail.com>
2022-04-23 22:05:32 -0300

    atualizacao ate 22 horas dia 23

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,1 +1,23 @@
-console.log("tudo certo")
+alert("Helloooo");
+
+function selecionarResposta (elemento) {
+    console.log(elemento);
+    
+    elemento.classList.remove("auxiliar");
+    elemento.querySelector(".opcao-resposta").classList.add("clicada");
+        
+    const pai = elemento.parentNode.parentNode;
+    console.log(pai);
+    
+    
+    for (let i = 0; i < 3; i++) {
+        let aux = pai.querySelector(".auxiliar");
+        if (aux !== null) {
+            aux.classList.remove("auxiliar");
+            aux.classList.add("naoclicada");
+            aux.querySelector(".opcao-resposta").classList.add("naoclicadafonte");
+        }
+    }
+}
+
+
