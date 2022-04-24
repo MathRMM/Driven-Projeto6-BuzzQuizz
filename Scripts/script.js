b08b694bf9cea668d695172b681bc68b9bbe3a8d
ruantoninatto <ruantoninatto@gmail.com>
2022-04-24 16:09:48 -0300

    atualizacao 24 04 16:00

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,10 +1,87 @@
-alert("Helloooo");
+const imagemexemplo = "https://i0.wp.com/www.popsfera.com.br/wp-content/uploads/2020/06/rmcapa.jpg?resize=800%2C445&ssl=1";
+const imagemexemplodois = "https://img.quizur.com/f/img616d7cd648db87.18180838.jpeg?lastEdited=1634565339";
+const quizzes = [
+    { objeto: [{
+        imagem: imagemexemplo,
+        respostastatus: "correta"
+        },
+        {
+        imagem: imagemexemplo,
+        respostastatus: "errada"
+        },
+        {
+        imagem: imagemexemplo,
+        respostastatus: "errada"
+        },
+        {
+        imagem: imagemexemplo,
+        respostastatus: "errada"
+        }
+    ] },
+    { objeto: [{ 
+        imagem: imagemexemplodois,
+        respostastatus: "correta"
+        },
+        {
+        imagem: imagemexemplodois,
+        respostastatus: "errada"
+        },
+        {
+        imagem: imagemexemplodois,
+        respostastatus: "errada"
+        },
+        {
+        imagem: imagemexemplodois,
+        respostastatus: "errada"
+        }    
+    ]}];
+
+console.log(quizzes[1].objeto[0].respostastatus);
+console.log(quizzes.length);
+
+
+let adicionarQuizz = document.querySelector(".caixa-quizz");
+
+for (j = 0; j < quizzes.length; j++) {
+
+    let sorteador = quizzes[j].objeto.sort(comparador);
+
+    adicionarQuizz.innerHTML = `<div class="caixa-pergunta corum"><h2>Qualquer pergunta tosca, aqui?</h2></div>
+    <div class="caixa-duas-opcoes um">
+      
+    </div>
+    <div class="caixa-duas-opcoes dois">
+      
+      </div>` + adicionarQuizz.innerHTML;
+
+
+    let adicionarPerguntasum = document.querySelector(".um");
+    console.log(adicionarQuizz.innerHTML);
+    for (let i = 0; i < 2; i++) {  
+    
+    adicionarPerguntasum.innerHTML += `<div class="caixa-opcao auxiliar" onclick="selecionarResposta(this)">
+    <img src=${sorteador[i].imagem}/>
+   <p class="opcao-resposta ${sorteador[i].respostastatus} oculta">Resposta ${i}</p>`
+    }
+    console.log(adicionarPerguntasum.innerHTML);
+    let adicionarPerguntasdois = document.querySelector(".dois")
+    for (let i = 2; i < 4; i++) {
+    
+    adicionarPerguntasdois.innerHTML += `<div class="caixa-opcao auxiliar" onclick="selecionarResposta(this)">
+    <img src=${sorteador[i].imagem}/>
+    <p class="opcao-resposta ${sorteador[i].respostastatus} oculta">Resposta ${i}</p>`;
+      
+    }
+    console.log(adicionarQuizz.innerHTML);
+    console.log(adicionarPerguntasdois.innerHTML);
+
+}
 
 function selecionarResposta (elemento) {
     console.log(elemento);
     
     elemento.classList.remove("auxiliar");
-    elemento.querySelector(".opcao-resposta").classList.add("clicada");
+    elemento.querySelector(".opcao-resposta").classList.remove("oculta");
         
     const pai = elemento.parentNode.parentNode;
     console.log(pai);
@@ -15,9 +92,10 @@ function selecionarResposta (elemento) {
         if (aux !== null) {
             aux.classList.remove("auxiliar");
             aux.classList.add("naoclicada");
-            aux.querySelector(".opcao-resposta").classList.add("naoclicadafonte");
+            aux.querySelector(".opcao-resposta").classList.remove("oculta");
         }
     }
 }
-
-
+function comparador() { 
+	return Math.random() - 0.5; 
+}
